57afbe39d54741dcb4deb19c88cc96de77746bcf
Jeffrey Dekker <jeffreydekker@JDSNA.local>
2023-02-08 04:47:31 +0100

    OOP php tutorial progress 9/18

--- OOP-php-tutorial/includes/person.inc.php
+++ OOP-php-tutorial/includes/person.inc.php
@@ -1,6 +1,42 @@
 <?php
 
 class Person {
-     
 
+    // Properties
+    private $name;
+    private $eyeColor;
+    private $age;
+
+    // static properties are the same for every instance of the class
+    public static $drinkingAge = 21;
+
+    // Constructors
+    public function __construct($name, $eyeColor, $age) {
+        $this->name = $name;
+        $this->eyeColor = $eyeColor;
+        $this->age = $age;
+    }
+
+    //Methods
+    public function setName($name){
+        $this->name = $name; // "this" references to this class and it's properties
+    }
+
+    public function getName() {
+        return $this->name;
+    }
+
+    // when dealing with statics, we use "self" instead of "this"
+    public static function getDA() {
+        return self::$drinkingAge;
+    }
+    // 
+    public static function setDrinkingAge($newDA) {
+        self::$drinkingAge = $newDA;
+    }
+
+    // destructor can delete an instance of the class
+    public function __destruct() {
+
+    }
 }
--- OOP-php-tutorial/index.php
+++ OOP-php-tutorial/index.php
@@ -1,3 +1,9 @@
+<?php
+
+// include the class, it's properties and methods for usage
+include 'includes/person.inc.php';
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,6 +13,33 @@
     <title>OOP PHP tutorial</title>
 </head>
 <body>
+
+    <?php
+        // "new Person" creates a new instance of the class "Person". We also
+        // give it some property values for it's constructor
+        $person1 = new Person("Jeffrey", "Green", 28);
+
+        // :: references to a static property of a class
+        echo Person::$drinkingAge;
+
+        // the below static method will update a static property
+        Person::setDrinkingAge(18);
+
+        // echo out the updated static property
+        echo Person::$drinkingAge;
+
+        Person::getDA();
+
+        // will give error because of private property 
+        echo $person1->name;
+
+        // unset function will delete the instance of a class 
+        unset($person1);
+
+        // this will not give an error because the method is public, but it will give 
+        // an error because the instance of the class no longer exists
+        echo $person1->getName();
+    ?>
 
 </body>
 </html>
